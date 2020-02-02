7cd1f49b79b49d1ef65c2679a8ec39d4d132dc50
Wing Kwong <wkwong@delta-y.co>
2020-02-01 21:00:36 -0600

    Added fancy plot.

--- scripts/superimportantscript.R
+++ scripts/superimportantscript.R
@@ -7,9 +7,23 @@ z = x + y
 a = x*y
 
 
-# Wing's R Script Changes
-library(magrittr)
-Sys.time() %>% print()
+
+####  Wing's R Script Changes  ####
+
+print(Sys.time())
+
+theta = seq(0, pi, length = 300)
+x <- cos(theta)
+y <- sin(theta)
+op <- par(bg = "black", mar = rep(0.5, 4))
+plot(x, y, type = 'n')
+segments(rep(0, 299), rep(0, 299), x[1:299] * runif(299, 0.7), 
+         y[1:299] * runif(299, 0.7), 
+         col = hsv(runif(299, 0.45, 0.55), 1, 1, runif(299, 0.5)), 
+         lwd = 5*runif(299))
+
+
+###################################
 
 
 a = x*y
