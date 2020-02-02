9cd71cff88e84efeed95690313b9cd0a3de15086
Wing Kwong <wkwong@delta-y.co>
2020-02-01 22:37:15 -0600

    Updated fancy plot code.

--- scripts/superimportantscript.R
+++ scripts/superimportantscript.R
@@ -12,10 +12,10 @@ a = x*y
 
 print(Sys.time())
 
-theta = seq(0, pi, length = 300)
-x <- cos(theta)
-y <- sin(theta)
-op <- par(bg = "black", mar = rep(0.5, 4))
+theta <- seq(0, pi, length = 300)
+x     <- cos(theta)
+y     <- sin(theta)
+par(bg = "black", mar = rep(0.5, 4))
 plot(x, y, type = 'n')
 segments(rep(0, 299), rep(0, 299), x[1:299] * runif(299, 0.7), 
          y[1:299] * runif(299, 0.7), 
